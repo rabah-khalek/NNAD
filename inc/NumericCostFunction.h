9d7f6e26296b7514b6101cc337a47f765097f5b8
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2019-03-03 01:52:46 +0100

    Optimization 1b

--- inc/NumericCostFunction.h
+++ inc/NumericCostFunction.h
@@ -8,21 +8,24 @@
 #include <ceres/ceres.h>
 #include "FeedForwardNN.h"
 
-
 // Typedef for the data
-typedef std::tuple<double,double,double> Datapoint;
+typedef std::tuple<double, double, double> Datapoint;
 typedef std::vector<Datapoint> vectdata;
 
-struct NumericCostFunction {
+struct NumericCostFunction
+{
+  ~NumericCostFunction();
+
   NumericCostFunction(int const &,
                       vectdata const &,
                       std::vector<int> const &,
                       int const &);
 
-  bool operator()(double const* const* , double* ) const;
-  
-    int _Np;
-    vectdata _Data;
-    std::vector<int> _NNarchitecture;
-    int _Seed;
+  bool operator()(double const *const *, double *) const;
+
+  FeedForwardNN<double> *_nn;
+  int _Np;
+  vectdata _Data;
+  std::vector<int> _NNarchitecture;
+  int _Seed;
 };
--- src/NumericCostFunction.cc
+++ src/NumericCostFunction.cc
@@ -4,25 +4,27 @@
 
 #include "NumericCostFunction.h"
 
+NumericCostFunction::~NumericCostFunction(){delete _nn;}
+
 NumericCostFunction::NumericCostFunction(int const &Np,
-                                       vectdata const &Data,
-                                       std::vector<int> const &NNarchitecture,
-                                       int const &Seed) : _Np(Np),
-                                                          _Data(Data),
-                                                          _NNarchitecture(NNarchitecture),
-                                                          _Seed(Seed)
-{}
+                                         vectdata const &Data,
+                                         std::vector<int> const &NNarchitecture,
+                                         int const &Seed) : _Np(Np),
+                                                            _Data(Data),
+                                                            _NNarchitecture(NNarchitecture),
+                                                            _Seed(Seed)
+{
+    _nn = new FeedForwardNN<double>(_NNarchitecture, _Seed);
+}
 
 bool NumericCostFunction::operator()(double const *const *parameters, double *residuals) const
 {
-    //TODO: pass the info from main
-    FeedForwardNN<double> *nn = new FeedForwardNN<double>(_NNarchitecture, _Seed);
     std::vector<double> pars;
     for (int i = 0; i < _Np; i++)
     {
         pars.push_back(parameters[i][0]);
     }
-    nn->SetParameters(pars);
+    _nn->SetParameters(pars);
     // Set parameters of the NN
 
     for (int id = 0; id < _Data.size(); id++)
@@ -30,10 +32,9 @@ bool NumericCostFunction::operator()(double const *const *parameters, double *re
         std::vector<double> input;
         double x = std::get<0>(_Data[id]);
         input.push_back(x);
-        const std::vector<double> v = nn->Evaluate(input);
+        const std::vector<double> v = _nn->Evaluate(input);
         residuals[id] = (v[0] - std::get<1>(_Data[id])) / std::get<2>(_Data[id]);
     }
 
-    delete nn;
     return true;
 }
