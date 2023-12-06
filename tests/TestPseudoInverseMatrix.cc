a9fd5c48e8fe57ef86d4947ca8ef85dd1d00d647
Valerio Bertone <valerio.bertone@cern.ch>
2023-12-06 20:00:38 +0100

    Tiny adjustment

--- tests/TestPseudoInverseMatrix.cc
+++ tests/TestPseudoInverseMatrix.cc
@@ -9,26 +9,26 @@
 int main()
 {
   nnad::Matrix<double> X{3, 3, std::vector<double>{1., 2., 3., 0., 4., 5., 1., 0., 6.}};
-  std::cout<<"Matrix = "<<std::endl;
+  std::cout << "Matrix = " << std::endl;
   X.Display();
 
   nnad::Matrix<double> CoFactor = X.GetCofactor(0, 0);
-  std::cout << "cofcator(0,0) = " << std::endl;
+  std::cout << "Cofactor(0,0) = " << std::endl;
   CoFactor.Display();
 
   double determinant = X.Determinant();
-  std::cout << "determinant = " << determinant << std::endl;
+  std::cout << "Determinant = " << determinant << std::endl;
 
   nnad::Matrix<double> adjoint = X.Adjoint();
-  std::cout << "adjoint = " << std::endl;
+  std::cout << "Adjoint = " << std::endl;
   adjoint.Display();
 
   nnad::Matrix<double> inverse = X.Inverse();
-  std::cout << "inverse = " << std::endl;
+  std::cout << "Inverse = " << std::endl;
   inverse.Display();
 
   nnad::Matrix<double> pseudoinverse = X.PseudoInverse_LLR();
-  std::cout << "pseudoinverse = " << std::endl;
+  std::cout << "Pseudoinverse = " << std::endl;
   pseudoinverse.Display();
 
   nnad::Matrix<double> X2{5, 13, std::vector<double>{0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,
@@ -37,11 +37,11 @@ int main()
                 0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,
                 0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0}};
 
-  std::cout<<"Matrix 2 = "<<std::endl;
+  std::cout << "Matrix 2 = " << std::endl;
   X2.Display();
 
   nnad::Matrix<double> pseudoinverse2 = X2.PseudoInverse_LLR();
-  std::cout << "pseudoinverse 2 = " << std::endl;
+  std::cout << "Pseudoinverse 2 = " << std::endl;
   pseudoinverse2.Display();
 
   return 0;
