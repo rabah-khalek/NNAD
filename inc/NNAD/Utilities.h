5a0a18ba3cc8c01d85208e51395f6c3442990422
achiefa <amedeochiefa@gmail.com>
2024-03-05 12:08:17 +0000

    Implementing tanh

--- inc/NNAD/Utilities.h
+++ inc/NNAD/Utilities.h
@@ -22,6 +22,18 @@ namespace nnad
     return s * (T(1) - s);
   }
 
+  template <typename T> T Tanh(T const &x)
+  {
+    const T e = exp( T(2) * x );
+    return ( e - T(1) ) / ( e + T(1) );
+  }
+
+  template <typename T> T dTanh(T const &x)
+  {
+    const T t = Tanh(x);
+    return T(1) - t * t;
+  }
+
   inline void Error(std::string const &Message)
   {
     std::cout << "\033[1;31mERROR: " << Message << "\033[0m" << std::endl;
